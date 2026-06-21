ac48b733bb9589b3d8056f254bcb753464847037
Shubhang Kuber <shubhangkuber@gmail.com>
2026-06-21 12:08:19 +0530

    deleted the files of client

--- client/src/components/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="flex items-center justify-center py-20">
-      <div className="w-8 h-8 rounded-full border-2 border-transparent animate-spin"
-           style={{ borderTopColor: '#f06420', borderRightColor: '#f0902044' }} />
-    </div>
-  )
-}
